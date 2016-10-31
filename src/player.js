bcf6992b42a4e3ba0b079bbca5afa6fd77e8d6fc
Gabriel Lebec <glebec@gmail.com>
2016-10-31 04:53:18 -0400

    remove link to gist (#17)
    Closes #17.

--- src/player.js
+++ src/player.js
@@ -10,9 +10,8 @@ This file has no test specs. It might be a tricky one. You need to look at
 the docs for the inquirer npm package, and see if you can figure out how
 to make the game run!
 
-If you're running out of time, check out our solution to the problem:
-
-https://gist.github.com/joedotjs/ae54507f758988222ce1c80fc3ba83e2
+If you're running out of time, check out our solution to the problem in the
+workshop text. But we hope you try it yourself first. :-)
 
 **/
 
